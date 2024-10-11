04b126ada09d45e9fdaa0f65cf61eb42552f38e3
Omar_CC <omars_skinny@hotmail.com>
2024-10-10 18:03:33 -0600

    App Web Rick And Morty Beta

--- RickAndMorty/src/app/app.component.ts
+++ RickAndMorty/src/app/app.component.ts
@@ -10,41 +10,21 @@ export class AppComponent {
   title = 'RickAndMorty';
 
   characters: any[] = [];
+  currentIndex: number = 2;
 
-  constructor(private data: RickmortyapiService){}
+  constructor(private data: RickmortyapiService) { }
 
-  ngOnInit(): void{
-    this.getCharacters()
-  }
-
-  images = [
-    'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
-    'https://via.placeholder.com/400x300/222222/FFFFFF?text=Foto+2',
-    'https://via.placeholder.com/400x300/333333/FFFFFF?text=Foto+3',
-  ];
-  index = 0;
-
-  nextImage() {
-    if (this.index < this.images.length - 1) {
-      this.index++;
-    } else {
-      this.index = 0; // Reinicia el carrusel al final
-    }
-  }
-
-  prevImage() {
-    if (this.index > 0) {
-      this.index--;
-    } else {
-      this.index = this.images.length - 1; // Va al final si está en el inicio
-    }
-  }
-
-  getCharacters(){
-    this.data.getCharacters().subscribe((data) =>{
-      this.characters = data;
-      console.log(data);
+  ngOnInit(): void {
+    this.data.getCharacters().subscribe((data) => {
+      this.characters = data.results
     })
+
+  }
+  prev(): void {
+    this.currentIndex = (this.currentIndex > 0) ? this.currentIndex - 1 : this.characters.length - 1;
   }
 
+  next(): void {
+    this.currentIndex = (this.currentIndex < this.characters.length - 1) ? this.currentIndex + 1 : 0;
+  }
 }
